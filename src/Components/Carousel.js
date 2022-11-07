3bffb4f0d2799fa8cd39c1c2695c734f6b47c3f1
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-11-07 13:55:52 +0200

    added carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,3 +1,47 @@
+import { useState } from "react";
+import "./Carousel.css";
+const images = [
+  { image: "carouselImages/image-slide-1.jpg", id: 0 },
+  { image: "carouselImages/image-slide-2.jpg", id: 1 },
+  { image: "carouselImages/image-slide-3.jpg", id: 2 },
+  { image: "carouselImages/image-slide-4.jpg", id: 3 },
+  { image: "carouselImages/image-slide-5.jpg", id: 4 },
+];
 export default function Carousel() {
-  return <div>Hello</div>;
+  const [location, setLocation] = useState(1);
+  const length = images.length;
+  function checkClass(number) {
+    if (location === number) {
+      return "current";
+    } else if (location - number === 1) {
+      return "previous";
+    } else if (location - number === -1) {
+      return "next";
+    } else {
+      return "hidden";
+    }
+  }
+  return (
+    <div>
+      <section className="carousel">
+        {images.map((elem) => (
+          <img
+            src={elem.image}
+            className={`image ${checkClass(elem.id)}`}
+            key={elem.id}
+          />
+        ))}
+      </section>
+      <button
+        onClick={() => setLocation(location === 1 ? length : location - 1)}
+      >
+        Previous
+      </button>
+      <button
+        onClick={() => setLocation(location === length - 1 ? 0 : location + 1)}
+      >
+        Next
+      </button>
+    </div>
+  );
 }
